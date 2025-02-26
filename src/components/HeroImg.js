ec828aae4366a0aa45296996d7f8cec6757ec28e
Radovan <radosce0@gmail.com>
2025-02-26 14:06:10 +0100

    HeroImg and styles

---
+++ src/components/HeroImg.js
@@ -0,0 +1,24 @@
+import "./HeroImgStyles.css"
+
+import React from 'react'
+
+import IntroImg from "../assets/intro-bg.jpg"
+
+import { Link } from "react-router-dom";
+
+const HeroImg = () => {
+  return (
+  <div className="hero">
+    <div className="mask">
+      <img className="into-img"
+      src={IntroImg} alt="IntroImg" />
+    </div>
+    <div className="content">
+      <h1>Plats för nya möjligheter</h1>
+      <p>Vi är det breda expertföretaget inom ...</p>
+    </div>
+ </div>
+  );
+};
+
+export default HeroImg;
