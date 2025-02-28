161917531e4712d3b86de5d00936dbdabbb5cf4f
Filippos <radovan@Filipposs-MacBook-Pro.local>
2025-02-28 14:03:00 +0100

    Add Work card

---
+++ src/components/WorkCardData.js
@@ -0,0 +1,23 @@
+import pro1 from '../assets/project1.jpg';
+import pro2 from '../assets/project2.jpg';
+import pro3 from '../assets/project3.jpg';
+
+const ProjectCardData = [
+    {
+        imgsrc: pro1,
+        title: "Ecommerce Website Design",
+        text: "This is a first project description",
+    },
+    {
+        imgsrc: pro2,
+        title: "Ecommerce Website Design",
+        text: "This is a second project description",
+    },
+    {
+        imgsrc: pro3,
+        title: "Ecommerce Website Design",
+        text: "This is a third project description",
+    },
+]   
+
+export default ProjectCardData;
